0b1eeb940a1e46f4279d552f987602a9a46d9c46
Matheus Henrique <matherique@gmail.com>
2021-07-03 20:58:00 -0300

    add user-data and refactory folder struct

---
+++ api/src/domain/user/user-data.ts
@@ -0,0 +1,9 @@
+export type UserData = {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  username: string;
+};
+
+ 
---
+++ api/src/domain/user/user.ts
@@ -0,0 +1,22 @@
+export class User {
+  public name: string;
+  public email: string;
+  public username: string;
+  public password: string;
+
+  constructor(name: string, email: string, username: string, password: string) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    Object.freeze(this);
+  }
+
+  static create(name: string, email: string, username: string, password: string) {
+    return new User(name, email, username, password)
+  }
+
+  validate(): boolean {
+    return true;
+  }
+};
