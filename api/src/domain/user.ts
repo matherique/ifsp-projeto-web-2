70d597aa46b06b00fce7815b2d635941c7625049
Matheus Henrique <matherique@gmail.com>
2021-07-03 19:27:07 -0300

    create register-user usecase

--- api/src/domain/user.ts
+++ api/src/domain/user.ts
@@ -1,6 +1,37 @@
-export type User = {
-  name: string;
-  email: string;
-  username: string;
-  password: string;
+export class User {
+  public name: string;
+  public email: string;
+  public username: string;
+  public password: string;
+
+  constructor(
+    name: string,
+    email: string,
+    username: string,
+    password: string,
+  ) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    Object.freeze(this);
+  }
+
+  static create(
+    name: string,
+    email: string,
+    username: string,
+    password: string,
+  ) {
+    return new User(
+      name,
+      email,
+      username,
+      password,
+    )
+  }
+
+  validate(): boolean {
+    return true;
+  }
 };
--- api/src/usercase/ports/user-repository.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "../../domain";
-
-export interface UserRepository {
-  findOne: (id: string) => Promise<User>;
-  findAll: () => Promise<User[]>;
-  create: (data: User) => Promise<User>;
-}
