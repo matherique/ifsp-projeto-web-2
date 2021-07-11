407714eceb0c7b011510832760cf434550601eb8
Matheus Henrique <matherique@gmail.com>
2021-07-11 20:55:01 -0300

    finish layout template

--- web/src/components/layout.tsx
+++ web/src/components/layout.tsx
@@ -3,7 +3,10 @@ import styled from 'styled-components'
 import Image from 'next/image'
 
 import configIcon from '../../public/config.png'
+import signOutIcon from '../../public/sign-out.svg'
 import Logo from '@/components/logo'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const Container = styled.div`
   display: flex;
@@ -22,21 +25,101 @@ const Config = styled.div`
     font-weight: bold;
     margin: 0px 10px;
   }
+  & > span:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
 `
-const Main = styled.div``
+const IconSignOut = styled(Image)`
+  cursor: pointer;
+`
+
+const Main = styled.div`
+  flex: 1;
+  margin: 0 100px;
+`
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+
+  & > ul {
+    list-style-type: none;
+  }
+
+  & > ul > li {
+    float: left;
+    margin: 0 10px;
+  }
+`
+
+type StyledLinkProps = {
+  active?: boolean
+}
+
+const StyledLink = styled.a<StyledLinkProps>`
+  font-size: 20px;
+  font-weight: 500;
+  text-decoration: none;
+  color: ${props => (props.active ? 'var(--red)' : 'var(--black2)')};
+
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
 
 type LayoutProps = {
   children?: React.ReactNode
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const router = useRouter()
+
+  function handleSignOut() {
+    router.push('/')
+  }
+
   return (
     <Container>
       <Header>
-        <Logo width="50" height="50" />
+        <Menu>
+          <Logo width="50" height="50" onClick={() => router.push('/painel')} />
+          <ul>
+            <li>
+              <Link href="/painel" passHref>
+                <StyledLink active={router.pathname === '/painel'}>
+                  comparador
+                </StyledLink>
+              </Link>
+            </li>
+            <li>
+              <Link href="/painel/relatorio" passHref>
+                <StyledLink active={router.pathname === '/painel/relatorio'}>
+                  relatório
+                </StyledLink>
+              </Link>
+            </li>
+            <li>
+              <Link href="/painel/usuario" passHref>
+                <StyledLink active={router.pathname === '/painel/usuario'}>
+                  editar dados
+                </StyledLink>
+              </Link>
+            </li>
+          </ul>
+        </Menu>
         <Config>
-          <span>Matheus Henrique</span>
-          <Image src={configIcon} alt="config" />
+          <span onClick={() => router.push('/painel/usuario')}>
+            Matheus Henrique
+          </span>
+          <IconSignOut
+            src={signOutIcon}
+            alt="sair"
+            title="sair"
+            onClick={() => handleSignOut()}
+          />
         </Config>
       </Header>
       <Main>{children}</Main>
