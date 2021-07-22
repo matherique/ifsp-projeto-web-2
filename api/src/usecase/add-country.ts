3f943b51a222141e375de9d81cc74c89c2046200
Matheus Henrique <matherique@gmail.com>
2021-07-22 18:14:41 -0300

    add import country list by upload

--- api/src/usecase/add-country.ts
+++ api/src/usecase/add-country.ts
@@ -11,13 +11,22 @@ export class AddCountry implements AddCountryUsecase {
   async handle({ data }: AddCountryParams): Promise<AddCountryResponse> {
     const countryData = data.toString().split('\r\n').filter(Boolean).slice(1)
     // 'Country Code', 'Region', 'IncomeGroup', 'SpecialNotes', 'TableName'
-    for (const country of countryData) {
+    const newCountries: Country[] = []
+    for await (const country of countryData) {
       const [code, region, , , name] = country
         .split(',"')
-        .map(l => l?.replace(/"/g, ''))
-      console.log(name)
+        .map(l => l?.replace(/"/g, '').replace(/,/g, ''))
+      if (!!!region) continue
+
+      const newCountry = await this.countryRepository.add({
+        code,
+        name,
+        region
+      })
+
+      newCountries.push(newCountry)
     }
 
-    return null
+    return newCountries
   }
 }
